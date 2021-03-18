edb1742823243fbd3fa186a54637f1e83e5614c1
Matrus <matrus@student.21-school.ru>
2021-03-19 02:20:07 +0300

    fractal finished

--- SimpleFractal/main.c
+++ SimpleFractal/main.c
@@ -1,6 +1,8 @@
 #define GL_SILENCE_DEPRECATION
 #include <GLUT/glut.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <math.h>
 #include "utils.h"
 
 static const GLint windowWidth =  640;
@@ -35,29 +37,111 @@ void triangle(GLfloat r, GLfloat g, GLfloat b) {
 	glEnd();
 }
 
-void drawTriangles(GLint count, GLfloat scale) {
+void line() {
+	glColor3b(0, 0, 0);
+	glBegin(GL_LINES);
+	glVertex2d(0,0);
+	glVertex2d(1,0);
+	glEnd();
+}
+
+void drawTunnel(GLint count, GLfloat scale, GLfloat * colors, GLint max) {
 	if (count == 0)
 		return;
 
 	glScalef(scale, scale, 0.f);
-	triangle(randFloat(), randFloat(), randFloat());
-	glRotatef(30.f, 0.f, 0.f, 1.f);
+	triangle(colors[(max - count) * 3], colors[(max - count) * 3 + 1], colors[(max - count) * 3 + 2]);
+	glRotatef(60.f, 0.f, 0.f, 1.f);
 	glTranslatef(1.f, 0.f, 0.f);
-	glRotatef(120.f, 0.f, 0.f, 1.f);
-	drawTriangles(count - 1, scale / 2);
+	glRotatef(180.f, 0.f, 0.f, 1.f);
+	glScalef(1.f / scale, 1.f / scale, 0.f);
+	drawTunnel(count - 1, scale * 0.9f, colors, max);
 }
+
+void drawTriangles(GLint count, GLfloat scale, GLfloat * colors, GLint max) {
+	if (count == 0)
+		return;
+	GLfloat r = colors[(max - count) * 3];
+	GLfloat g = colors[(max - count) * 3 + 1];
+	GLfloat b = colors[(max - count) * 3 + 2];
+// left triangle
+	glPushMatrix();
+	glScalef(scale / 2.f, scale / 2.f, 1.f);
+	triangle(r, g, b);
+	glPopMatrix();
+	glPushMatrix();
+	drawTriangles(count - 1, scale / 2.f, colors, max);
+	glPopMatrix();
+// top triangle
+	glPushMatrix();
+	glScalef(scale / 2.f, scale / 2.f, 1.f);
+	glRotatef(60.f, 0.f, 0.f, 1.f);
+	glTranslatef(1.f, 0.f, 0.f);
+	glRotatef(-60.f, 0.f, 0.f, 1.f);
+	triangle(r, g, b);
+	glScalef(2.f / scale, 2.f / scale, 1.f);
+	drawTriangles(count - 1, scale / 2.f, colors, max);
+	glPopMatrix();
+//	 right triangle
+	glPushMatrix();
+	glScalef(scale / 2.f, scale / 2.f, 0.f);
+	glTranslatef(1.f, 0.f, 0.f);
+	triangle(r, g, b);
+	glScalef(2.f / scale, 2.f / scale, 0.f);
+	drawTriangles(count - 1, scale / 2.f, colors, max);
+	glPopMatrix();
+//	 mid triangle
+//	glPushMatrix();
+//	glScalef(scale / 2.f, scale / 2.f, 0.f);
+//	glTranslatef(1.f, 0.f, 0.f);
+//	glRotatef(60.f, 0.f, 0.f, 1.f);
+//	triangle(r, g, b);
+//	glScalef(2.f / scale, 2.f / scale, 0.f);
+//	drawTriangles(count - 1, scale / 2.f, colors, max);
+//	glPopMatrix();
+}
+
+GLfloat * reinitColors(GLfloat * colors, GLint count) {
+	if (colors)
+		free(colors);
+	colors = malloc(sizeof(GLfloat) * count * 3);
+	if (!colors)
+		exit_fatal("malloc fails");
+
+	for (int i = 0; i < count * 3; ++i) {
+		colors[i] = randFloat();
+	}
+	return colors;
+}
+
+static GLfloat angle;
+static GLfloat dAngle = 0.3f;
 
 void display() {
 	static GLint trianglesToDisplay;
-	static GLint maxTrianglesToDisplay = 20;
+	static GLint maxTrianglesToDisplay = 7;
+	static GLfloat * colors;
+	if (!colors)
+		colors = reinitColors(colors, maxTrianglesToDisplay);
 
 	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-	drawTriangles(1, windowHeight * 0.9f);
-	glPopMatrix();
+	for (int i = 0; i < 3; ++i) {
+		glPushMatrix();
+		glRotatef(angle + i * 120.f, 0.f, 0.f, 1.f);
+		drawTriangles(trianglesToDisplay + 1, windowHeight * 0.4f, colors, trianglesToDisplay + 1);
+		glPopMatrix();
+	}
+
+	angle += dAngle;
+	if (angle >= 360.f) {
+		angle = 0.f;
+	}
+	if ( (int)angle % 10 == 0)
+		++trianglesToDisplay;
 
 	if (trianglesToDisplay == maxTrianglesToDisplay) {
 		trianglesToDisplay = 1;
+		colors = reinitColors(colors, maxTrianglesToDisplay);
 	}
 
 	glFlush();
